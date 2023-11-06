a0c6ee3412008ffa998a095fbd6675a731c55860
Neeraj Kumar <testingdach@gmail.com>
2023-11-06 20:57:12 +0530

    File is also added

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-console.log("hello World") ;
-console.log("hello World2") ;
-console.log("hello world 3 ") ;
-console.log("harry potter") ;
-console.log("harry potter 3") ;
+console.log(hello)
+console.log(hello) 
